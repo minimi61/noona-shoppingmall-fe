912c2e212eb1a0c5c0a2dbc6ba6521816c208421
minimi61 <new1573@naver.com>
2024-06-04 22:02:02 +0900

    feat: 아이템 생성

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,8 +6,8 @@ const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
   //TODO:
-  // baseURL: LOCAL_BACKEND,
-  baseURL: `${BACKEND_PROXY}/api`,
+  baseURL: LOCAL_BACKEND,
+  // baseURL: `${BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
